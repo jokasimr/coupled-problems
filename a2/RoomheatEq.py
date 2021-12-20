0ce391086f7a48a86db25e37331ba582cb1eb8fe
Johannes Kasimir <johannes.kasimir@math.lu.se>
2021-12-20 17:53:19 +0100

    update assignment 2 code

--- a2/RoomheatEq.py
+++ a2/RoomheatEq.py
@@ -11,21 +11,37 @@ from solver.laplace import LaplaceOnRectangle, HeatEqOnRectangle
 
 h = 1/20
 
-dn_iter = 50
+dn_iter = 300
 tol_wr = 1e-10
-T_start =0
-T_end = 100
-dt = 0.1
+T_start = 0
+T_end = 10000
+dt = 1000
 
-u0_l = np.ones([(int(1/h)+1)**2])*210
-u0_m = np.ones([2*(int(1/h)+1)**2])*220
-u0_r = np.ones([(int(1/h)+1)**2])*210
+WALL = 210
+WINDOW = 200
+HEATER = 230
 
-left = HeatEqOnRectangle(u0_l,dt,h, 1, 1, lambda x, y: x*0)
-#middle = HeatEqOnRectangle(u0_m,dt,h, 1, 2, lambda x, y: x*0,alpha = 1300,lamda = 0.0243)
-middle = HeatEqOnRectangle(u0_m,dt,h, 1, 2, lambda x, y: x*0,alpha = 1,lamda = 1)
+u0_l = np.vectorize(lambda x, y, q=1e-5: (
+    WALL if y < q or y > 1 - q else
+    HEATER if x < q else 
+    WALL
+))
+u0_m = np.vectorize(lambda x, y, q=1e-5: (
+    WALL if y > 2 - q else
+    WALL if x < q and y >= 1/2 or x > 1 - q and y <= 1/2 else
+    WINDOW if y < q else
+    220 
+))
+u0_r = np.vectorize(lambda x, y, q=1e-5: (
+    WALL if y < q or y > 1 - q else
+    HEATER if x > 1 - q else 
+    WALL
+))
 
-right = HeatEqOnRectangle(u0_r,dt,h, 1, 1, lambda x, y: x*0)
+left = HeatEqOnRectangle(dt, h, 1, 1, lambda x, y: x*0, initial_condition=u0_l)
+middle = HeatEqOnRectangle(dt, h, 1, 2, lambda x, y: x*0, initial_condition=u0_m, heat_capacitance=1300, heat_conductivity=0.0243)
+#middle = HeatEqOnRectangle(dt, h, 1, 2, lambda x, y: x*0, initial_condition=u0_m, heat_capacitance=1, heat_conductivity=1)
+right = HeatEqOnRectangle(dt, h, 1, 1, lambda x, y: x*0, initial_condition=u0_r)
 
 def reset():
     
@@ -35,9 +51,9 @@ def reset():
 
     left_heater = left.geometry.boundary_dofs(3)[1:-1]
 
-    left.set_dirchlet(0, lambda x, y: x*0 + 210)
-    left.set_dirchlet(left_heater, lambda x, y: x*0 + 230)
-    left.set_dirchlet(2, lambda x, y: x*0 + 210)
+    left.set_dirchlet(0, lambda x, y: x*0 + WALL)
+    left.set_dirchlet(left_heater, lambda x, y: x*0 + HEATER)
+    left.set_dirchlet(2, lambda x, y: x*0 + WALL)
 
     middle_window = middle.geometry.boundary_dofs(0)[1:-1]
     middle_right_wall = middle.geometry.boundary_dofs(1)
@@ -49,13 +65,13 @@ def reset():
         middle_left_wall[:len(middle_left_wall) // 2 + 1],
     ])
 
-    middle.set_dirchlet(middle_window, lambda x, y: x*0 + 200)
-    middle.set_dirchlet(middle_wall, lambda x, y: x*0 + 210)
+    middle.set_dirchlet(middle_window, lambda x, y: x*0 + WINDOW)
+    middle.set_dirchlet(middle_wall, lambda x, y: x*0 + WALL)
 
     right_heater = right.geometry.boundary_dofs(1)[1:-1]
-    right.set_dirchlet(0, lambda x, y: x*0 + 210)
-    right.set_dirchlet(right_heater, lambda x, y: x*0 + 230)
-    right.set_dirchlet(2, lambda x, y: x*0 + 210)
+    right.set_dirchlet(0, lambda x, y: x*0 + WALL)
+    right.set_dirchlet(right_heater, lambda x, y: x*0 + HEATER)
+    right.set_dirchlet(2, lambda x, y: x*0 + WALL)
 
 
 # left left
@@ -68,43 +84,79 @@ rm_interface = middle.geometry.boundary_dofs(1)[len(middle.geometry.boundary_dof
 rr_interface = right.geometry.boundary_dofs(3)[1:-1][::-1]
 
 
-u_gamma_left = 15 * np.ones(len(ll_interface))
-u_gamma_right = 15 * np.ones(len(rr_interface))
+u_gamma_left = WALL * np.ones(len(ll_interface))
+u_gamma_right = WALL * np.ones(len(rr_interface))
 
-theta = 0.5
-time_steps = int((T_end-T_start)/dt)+1
-time_steps = 100
+#theta = 0.0415
+#theta = 0.0234/(1+0.0234)
+theta = 0.0234
+time_steps = int((T_end - T_start) // dt)
+
 for j in range(time_steps):
     for i in range(dn_iter):
 
         reset()
-        left.set_dirchlet(ll_interface, u_gamma_left, raw=True)
-        right.set_dirchlet(rr_interface, u_gamma_right, raw=True)
+
+        '''
+        middle.set_dirchlet(lm_interface, u_gamma_left, raw=True)
+        middle.set_dirchlet(rm_interface, u_gamma_right, raw=True)
+        middle.do_euler_step()
+
+        flux_left = middle.heat_flux_at_nodes(lm_interface)
+        flux_right = middle.heat_flux_at_nodes(rm_interface)
+
+        left.set_neumann(ll_interface, -flux_left, raw=True)
+        right.set_neumann(rr_interface, -flux_right, raw=True)
 
         left.do_euler_step()
         right.do_euler_step()
-        flux_left = left.heat_flux_at_nodes(ll_interface)
-        flux_right = right.heat_flux_at_nodes(rr_interface)
 
-        middle.set_neumann(lm_interface, flux_left, raw=True)
-        middle.set_neumann(rm_interface, flux_right, raw=True)
-        middle.do_euler_step()
+        ug = np.concatenate((u_gamma_left, u_gamma_right))
 
-        ug = np.concatenate((u_gamma_left,u_gamma_right))
-        ug_new = np.concatenate((middle.sol[lm_interface].copy(),middle.sol[rm_interface].copy()))
+        u_gamma_left = ((1 - theta) * u_gamma_left + theta * left.sol[ll_interface].copy())
+        u_gamma_right = ((1 - theta) * u_gamma_right + theta * right.sol[rr_interface].copy())
 
-        if np.linalg.norm(ug-ug_new,2)<tol_wr or i+1 == dn_iter:
+        ug_new = np.concatenate((u_gamma_left, u_gamma_right))
+
+        if np.linalg.norm(ug - ug_new, 2) < tol_wr or i + 1 == dn_iter:
             middle.update_u_old()
             left.update_u_old()
             right.update_u_old()
             print(i)
             break
 
+        '''
+
+        left.set_dirchlet(ll_interface, u_gamma_left, raw=True)
+        right.set_dirchlet(rr_interface, u_gamma_right, raw=True)
+
+        left.do_euler_step()
+        right.do_euler_step()
+
+        flux_left = left.heat_flux_at_nodes(ll_interface)
+        flux_right = right.heat_flux_at_nodes(rr_interface)
+
+        middle.set_neumann(lm_interface, -flux_left, raw=True)
+        middle.set_neumann(rm_interface, -flux_right, raw=True)
+
+        middle.do_euler_step()
+
+        ug = np.concatenate((u_gamma_left,u_gamma_right))
 
         u_gamma_left = ((1 - theta) * u_gamma_left + theta * middle.sol[lm_interface].copy())
         u_gamma_right = ((1 - theta) * u_gamma_right + theta * middle.sol[rm_interface].copy())
 
+        ug_new = np.concatenate((u_gamma_left, u_gamma_right))
+
+        if np.linalg.norm(ug - ug_new, 2) < tol_wr or i + 1 == dn_iter:
+            middle.update_u_old()
+            left.update_u_old()
+            right.update_u_old()
+            print(i)
+            break
+
         
+
 print(middle.sol)
 print(right.sol)
 print(left.sol)
@@ -136,7 +188,8 @@ plt.title('Temperature distribution in the room')
 #clean_figure()
 #os.makedirs("room", exist_ok=True)
 #save('room/room.tex')
-plt.savefig('room')
+#plt.savefig('room')
+plt.show()
 
 
 k = 75
@@ -163,5 +216,5 @@ plt.colorbar(orientation="horizontal", pad=0.2)
 plt.title('A closer look near the window')
 #clean_figure()
 #save('room/closer.tex')
-#plt.show()
-plt.savefig('A closer look near the window')
+plt.show()
+#plt.savefig('A closer look near the window')
